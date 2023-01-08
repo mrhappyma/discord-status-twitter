a7d1283fb2386a6b9fcac09d11e79cee1d5a4573
Dominic Ruggiero <dominic@userexe.me>
2023-01-08 13:11:10 -0500

    small update

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,10 +3,7 @@ declare global {
     interface ProcessEnv {
       DISCORD_TOKEN: string;
       DATABASE_URL: string;
-      TWITTER_API_KEY: string;
-      TWITTER_API_SECRET: string;
-      TWITTER_ACCESS_TOKEN: string;
-      TWITTER_ACCESS_TOKEN_SECRET: string;
+      TWITTER_TOKEN: string;
     }
   }
 }
